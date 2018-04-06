6c539ef8f9d5a2a4e496a46ddefa05e7fa857db3
Mostafa Yasin <mostafa.ys2014@gmail.com>
2018-04-06 21:53:32 +0200

    added material skin DLL

--- Graphics Project UI/Form1.cs
+++ Graphics Project UI/Form1.cs
@@ -7,14 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin.Controls;
 
 namespace Graphics_Project_UI
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
+
+
         }
     }
 }
